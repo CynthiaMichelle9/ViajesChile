eb6d894ad38328681f9a6450b49a946291714797
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2024-03-12 17:25:57 -0300

    Identación y documentación del proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
 /* Utilizando toggle en tarjetas para ocultar el texto haciendo click en el título */
-$(document).ready(function() {
-    $(".card-title").on("click", function() {
+$(document).ready(function () {
+    $(".card-title").on("click", function () {
         $(this).parent().find(".card-text").toggle();
     });
 });
 
 
-// Script para el desplazamiento suave
-$(document).ready(function(){
-    $('a[href^="#"]').on('click', function(e){
+/* Script para el desplazamiento suave desde el link de las secciones en la barra de navegación  */
+$(document).ready(function () {
+    $('a[href^="#"]').on('click', function (e) {
         e.preventDefault();
         var target = $(this).attr('href');
         $('html, body').animate({
