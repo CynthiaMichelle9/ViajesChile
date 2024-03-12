8afcc6983fae941d5520bb3546bc9f40b4ac6855
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2024-03-12 17:11:26 -0300

    Funcionalidad Smooth Scroll aplicada

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,13 @@ $(document).ready(function() {
 });
 
 
+// Script para el desplazamiento suave
+$(document).ready(function(){
+    $('a[href^="#"]').on('click', function(e){
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        }, 1000);
+    });
+});
